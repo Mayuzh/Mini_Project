4761a329f916c977818412df303c83f0aab20aef
Mona Zhao <70651849+Mayuzh@users.noreply.github.com>
2023-10-18 13:51:14 -0700

    move file

--- retrieveData.R
+++
@@ -1,8 +0,0 @@
-tuesdata <- tidytuesdayR::tt_load('2023-10-10')
-haunted_places <- tuesdata$haunted_places
-
-print(haunted_places)
-
-write.csv(x = haunted_places, file="haunted_places_data.csv")
-
-glimpse(haunted_places)
