5c5f32869f86d0573f7e64583e80f901cb2b1b6d
Ginna Khang <ginnakhang@gmail.com>
2023-11-27 14:29:51 -0800

    modfied sortData for MiniProject2

--- MiniProject2/sortData.R
+++ MiniProject2/sortData.R
@@ -22,21 +22,9 @@ for (i in 1:nrow(data)) {
   }
 }
 
-# Print the values of column 1 for selected entries
-print("Tragic Events:")
-print(tragic_events)
-print("Paranormal Events:")
-print(paranormal)
-print("Unexplainable Events:")
-print(unexplainable_events)
-
-
 # Load required libraries
 library(dplyr)
 library(ggplot2)
-
-# Read the CSV file
-data <- read.csv("data/haunted_places_data.csv")
 
 # Create a data frame with the count of events by state
 events_by_state <- data %>%
@@ -49,7 +37,7 @@ events_by_state <- data %>%
   summarize(count = n())
 
 # Create a line graph that separates each event type
-ggplot(events_by_state, aes(x = state_abbrev, y = count, color = event_type, group = event_type)) +
+eventsByState <- ggplot(events_by_state, aes(x = state_abbrev, y = count, color = event_type, group = event_type)) +
   geom_line() +
   geom_point() +
   labs(title = "Comparison of Events by State",
@@ -60,14 +48,45 @@ ggplot(events_by_state, aes(x = state_abbrev, y = count, color = event_type, gro
   theme(axis.text.x = element_text(angle = 90)) +
   scale_color_manual(values = c("Tragic Events" = "skyblue", "Paranormal Events" = "red", "Unexplainable Events" = "green"))
 
+ggsave(filename="images/US-events-by-states.png", 
+       plot = eventsByState, 
+       width = 50, height = 15, 
+       dpi = 300,
+       units = "cm")
+
 # Create a data frame with the count of tragic events by state
 tragic_events_by_state <- data %>%
+  filter(grepl("commited suicide|death|died|drowned|killed|murdered|murder", tolower(description))) %>%
+  group_by(state_abbrev) %>%
+  summarize(count = n())
+
+# Create a bar chart
+tragicEventsByState <- ggplot(tragic_events_by_state, aes(x = state_abbrev, y = count, fill = state_abbrev)) +
+  geom_bar(stat = "identity", width = 0.7) +
+  geom_text(aes(label = count), vjust = -0.5, size = 3) +
+  geom_hline(yintercept = 0, color = "black", size = 0.5) +
+  geom_vline(xintercept = 0, color = "black", size = 0.5) +
+  labs(title = "Number of Tragic Events by State",
+       x = "State",
+       y = "Count of Tragic Events") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 90, size = 7),
+        axis.text.y = element_text(size = 7))
+
+ggsave(filename="images/US-tragic-events-by-states.png", 
+       plot = tragicEventsByState, 
+       width = 50, height = 15, 
+       dpi = 300,
+       units = "cm")
+
+# Create a data frame with the count of tragic events by state
+paranormal_events_by_state <- data %>%
   filter(grepl("haunted|ghost|apparition|apparitions|ghostly|supernatural|paranormal|spirit", tolower(description))) %>%
   group_by(state_abbrev) %>%
   summarize(count = n())
 
 # Create a bar chart
-ggplot(tragic_events_by_state, aes(x = state_abbrev, y = count, fill = state_abbrev)) +
+paranormalEventsByState <- ggplot(paranormal_events_by_state, aes(x = state_abbrev, y = count, fill = state_abbrev)) +
   geom_bar(stat = "identity", width = 0.7) +
   geom_text(aes(label = count), vjust = -0.5, size = 3) +
   geom_hline(yintercept = 0, color = "black", size = 0.5) +
@@ -79,6 +98,12 @@ ggplot(tragic_events_by_state, aes(x = state_abbrev, y = count, fill = state_abb
   theme(axis.text.x = element_text(angle = 90, size = 7),
         axis.text.y = element_text(size = 7))
 
+ggsave(filename="images/US-paranormal-events-by-states.png", 
+       plot = paranormalEventsByState, 
+       width = 50, height = 15, 
+       dpi = 300,
+       units = "cm")
+
 # Create a data frame with the count of unexplainable events by state
 unexplainable_events_by_state <- data %>%
   filter(!grepl("commited suicide|death|died|drowned|killed|murdered|murder|haunted|ghost|apparition|apparitions|ghostly|supernatural|paranormal|spirit", tolower(description))) %>%
@@ -86,7 +111,7 @@ unexplainable_events_by_state <- data %>%
   summarize(count = n())
 
 # Create a bar chart for unexplainable events
-ggplot(unexplainable_events_by_state, aes(x = state_abbrev, y = count, fill = state_abbrev)) +
+unexplainableEventsByState <- ggplot(unexplainable_events_by_state, aes(x = state_abbrev, y = count, fill = state_abbrev)) +
   geom_bar(stat = "identity") +
   geom_text(aes(label = count), vjust = -0.5, size = 3) +
   geom_hline(yintercept = 0, color = "black", size = 0.5) +
@@ -98,6 +123,11 @@ ggplot(unexplainable_events_by_state, aes(x = state_abbrev, y = count, fill = st
   theme(axis.text.x = element_text(angle = 90, size = 7),
         axis.text.y = element_text(size = 7))
 
+ggsave(filename="images/US-unexplainable-events-by-states.png", 
+       plot = unexplainableEventsByState, 
+       width = 50, height = 15, 
+       dpi = 300,
+       units = "cm")
 
 # Create a data frame with the total count of events by event type
 total_events <- events_by_state %>%
@@ -105,7 +135,7 @@ total_events <- events_by_state %>%
   summarize(total_count = sum(count))
 
 # Create a bar graph to show the total count of events
-ggplot(total_events, aes(x = event_type, y = total_count, fill = event_type)) +
+totalCountofEvents <- ggplot(total_events, aes(x = event_type, y = total_count, fill = event_type)) +
   geom_bar(stat = "identity") +
   geom_text(aes(label = total_count), vjust = -0.5, size = 3) +
   labs(title = "Total Count of Events by Event Type",
@@ -115,13 +145,19 @@ ggplot(total_events, aes(x = event_type, y = total_count, fill = event_type)) +
   theme_minimal() +
   scale_fill_manual(values = c("Tragic Events" = "skyblue", "Paranormal Events" = "orange", "Unexplainable Events" = "green"))
 
+ggsave(filename="images/US-total-count-of-events.png", 
+       plot = totalCountofEvents, 
+       width = 50, height = 15, 
+       dpi = 300,
+       units = "cm")
+
 # Create a data frame with the count of haunted locations by state
 haunted_locations_by_state <- data %>%
   group_by(state_abbrev) %>%
   summarize(count = n())
 
 # Create a bar chart
-ggplot(haunted_locations_by_state, aes(x = state_abbrev, y = count, fill = state_abbrev)) +
+hauntedLocationsByState <- ggplot(haunted_locations_by_state, aes(x = state_abbrev, y = count, fill = state_abbrev)) +
   geom_bar(stat = "identity") +
   geom_text(aes(label = count), vjust = -0.5, size = 3) +  # Add labels on top of the bars
   labs(title = "Number of Haunted Locations by State",
@@ -131,7 +167,8 @@ ggplot(haunted_locations_by_state, aes(x = state_abbrev, y = count, fill = state
   theme(axis.text.x = element_text(angle = 90, size = 7),
         axis.text.y = element_text(size = 7))
 
-
-
-
-
+ggsave(filename="images/US-haunted-locations-by-state.png", 
+       plot = hauntedLocationsByState, 
+       width = 50, height = 15, 
+       dpi = 300,
+       units = "cm")
